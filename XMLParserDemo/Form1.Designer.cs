7b24551442e03e4eaf0dc43de435bf0b9b583bd4
student <student@PC_105_1->
2019-02-09 16:52:54 +0200

    release1

--- XMLParserDemo/Form1.Designer.cs
+++ XMLParserDemo/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.intersectionsListBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.intersectionsListBox);
+            this.panel1.Location = new System.Drawing.Point(13, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 222);
+            this.panel1.TabIndex = 1;
+            // 
+            // intersectionsListBox
+            // 
+            this.intersectionsListBox.FormattingEnabled = true;
+            this.intersectionsListBox.Location = new System.Drawing.Point(4, 4);
+            this.intersectionsListBox.Name = "intersectionsListBox";
+            this.intersectionsListBox.Size = new System.Drawing.Size(252, 212);
+            this.intersectionsListBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox intersectionsListBox;
     }
 }
 
--- XMLParserDemo/Form1.cs
+++ XMLParserDemo/Form1.cs
@@ -7,14 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace XMLParserDemo
 {
     public partial class Form1 : Form
     {
+        private int counter = 0;
+        private int maxCount = 100;
+        private List<string> names =
+            new List<string>() {
+                "Microsoft",
+                "Apple",
+                "Oracle",
+                "IBM",
+                "Яндекс",
+                "ТОВ \"ТЕХМЕТМАШ\"",
+                "Без имени"
+            };
+        BindingList<string> intersectionData = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
+            intersectionsListBox.DataSource = intersectionData;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = dialog.FileName;
+                //Console.WriteLine(filePath);
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(filePath);
+                //printNodes(xmlDocument);
+                /*XmlNode dataNode =
+                    ((XmlElement)xmlDocument.GetElementsByTagName("DATA")[0].ChildNodes[0]).GetElementsByTagName("NAME")[0];*/
+                //Console.WriteLine(dataNode.InnerText);
+                //this.Text = dataNode.InnerText;
+
+                XmlNodeList recordNodes =
+                    xmlDocument.GetElementsByTagName("DATA")[0].ChildNodes;
+
+                var intersectionsList =
+                    recordNodes.Cast<XmlNode>()
+                        .Select(record => ((XmlElement)record).GetElementsByTagName("NAME")[0].InnerText)
+                        .Intersect(names);
+
+                foreach (var item in intersectionsList)
+                {
+                    intersectionData.Add(item);
+                }
+
+                foreach (var item in intersectionData)
+                {
+                    Console.WriteLine(item);
+                }
+
+                
+            }
+        }
+
+        private void printNodes(XmlNode node) {
+            foreach (XmlNode item in node.ChildNodes)
+            {
+                if (counter < maxCount)
+                {
+                    Console.WriteLine(item.Name + " = " + item.Value);
+                    Console.Write("\t");
+                    if (item.HasChildNodes)
+                    {
+                        printNodes(item);
+                    }
+                    counter++;
+                }
+            }
         }
     }
 }
